7c41629a0bc0384652161537eacc6085576014aa
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-26 19:54:14 +0600

    news

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+         
+         
+          'c-purple': '#3f3cbb',
+         
+        },
+      },
     },
     plugins: [],
   }
